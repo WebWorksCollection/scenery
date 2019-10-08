19c360e75098c07cabe503297558758929f9f6aa
Jesse <jesse.greenberg@colorado.edu>
2019-10-08 09:56:49 -0400

    check for accessible Display

--- js/listeners/MultiPanZoomListener.js
+++ js/listeners/MultiPanZoomListener.js
@@ -182,7 +182,8 @@ define( require => {
      * @param {DOMEvent} domEvent
      */
     globalKeydown( domEvent ) {
-      if ( !phet.joist.sim.display.accessibleDOMElement.contains( domEvent.target ) ) {
+      const displayAccessible = phet.joist.sim.display._accessible;
+      if ( !displayAccessible || !phet.joist.sim.display.accessibleDOMElement.contains( domEvent.target ) ) {
         this.handleZoomCommands( domEvent );
 
         // handle translation without worry of the pointer being attached because there is no pointer at this level
